9a05c38ed90de7d51453e0db1b35a66358f3b501
Gustavo de Jesus Santos <gustavojspro@hotmail.com>
2019-02-21 19:51:37 -0300

    O que foi mudado

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,20 +2,34 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
-	public Sessao(Integer id, LocalTime horario, Sala sala, Filme filme) {
-		super();
-		this.id = id;
+	
+	
+	public Sessao(LocalTime horario, Sala sala, Filme filme) {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
 	}
+	
+	public Sessao() {}
 	
 	public Integer getId() {
 		return id;
